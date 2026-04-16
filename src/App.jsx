d8c4643785f6d0395f541bdc4d76f53875693366
Chuchuyen2021 <nongchuchuyen2021@gmail.com>
2026-04-16 16:24:11 +0700

    Nang cap giao dien Tab chuyên nghiệp

--- src/App.jsx
+++ src/App.jsx
@@ -1065,7 +1065,7 @@ export default function App() {
              {" "}
       </div>
             {/* Header Sticky */}     {" "}
-      <header className="bg-white/80 backdrop-blur-md shadow-sm cd Ecdrelative border-b border-gray-200">
+      <header className="bg-white/80 backdrop-blur-md shadow-sm relative border-b border-gray-200">
                {" "}
         <div className="max-w-5xl mx-auto px-4 py-3 flex flex-col md:flex-row justify-between items-center gap-4">
                    {" "}
